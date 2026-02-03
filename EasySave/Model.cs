9552f3cc50fcfc6dbedc3507b9b9154733b8f946
uvsq22000319 <malak.lalami@ens.uvsq.fr>
2026-02-03 15:15:48 +0100

    ajout de la classe BackUpJob et de ses attributs

--- EasySave/Model.cs
+++ EasySave/Model.cs
@@ -1,11 +1,28 @@
 ﻿using System;
-namespace EasySave
+namespace EasySave.Model
 {
-	public class Model
-	{
-		public Model()
-		{
-		}
-	}
+    public class BackUpJob
+    {
+        public string Name { get; set; } // Nom de la sauvegarde
+        public string SourceDir { get; set; } // Dossier source de la sauvegarde
+        public string TargetDir { get; set; } // Destination de la sauvegarde
+        public string BackUpType { get; set; } // Type de sauvegarde
+
+        // État en temps réel (évolutifs)
+        public string State { get; set; }      // "Active" ou "Inactive"
+        public int TotalFiles { get; set; }
+        public long TotalSize { get; set; }
+        public int Progress { get; set; }
+
+        public BackUpJob(string name, string source, string target, string type)
+        {
+            Name = name;
+            SourceDir = source;
+            TargetDir = target;
+            BackUpType = type;
+            State = "Inactive"; // Par défaut au démarrage
+            Progress = 0;
+        }
+    }
 }
 
