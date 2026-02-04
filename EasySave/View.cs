018af7882e204b34e312385a729072ccb175a54d
uvsq22000319 <malak.lalami@ens.uvsq.fr>
2026-02-04 15:11:33 +0100

    Corrections et ajout de commentaires supplémentaires

--- EasySave/View.cs
+++ EasySave/View.cs
@@ -5,27 +5,45 @@ using EasySave.ViewModel;
 
 namespace EasySave.View
 {
+    /// <summary>
+    /// Classe qui gère l'interface utilisateur en mode console.
+    /// Elle gère l'affichage et les saisies 
+    /// et les communique au ViewModel pour traiter les données.
+    /// </summary>
     public class ConsoleView
     {
+        // Reference vers le ViewModel 
         private MainViewModel _viewModel;
+
+
+        /// <summary>
+        /// Constructeur de la vue.
+        /// On reçoit le ViewModel par injection
+        /// pour permettre la communication entre l'interface et la logique
+        /// </summary>
+        /// <param name="viewModel">L'instance du ViewModel principal de l'application</param>
         public ConsoleView(MainViewModel viewModel)
         {
             _viewModel = viewModel;
         }
 
+        /// <summary>
+        /// Méthode qui est la boucle principale qui permet de lancer l'interface.
+        /// Elle gère l'affichage et dirige dans d'autres menus en fonction des choix utilisateur.
+        /// </summary>
         public void Start()
         {
             bool exit = false;
             while (!exit)
             {
-                // On affiche le menu selon la langue choisie dans le VM
+                // On affiche le menu selon la langue choisie, par défaut au démarrage,fr
                 ShowMenu();
 
                 string choice = Console.ReadLine()!;
                 switch (choice)
                 {
                     case "1":
-                        // Appelle une méthode pour saisir les infos du Job
+                        // Appelle une méthode pour saisir les infos du Job et le créer
                         CreateJobUI();
                         break;
                     case "2":
@@ -33,6 +51,7 @@ namespace EasySave.View
                         ExecuteJobUI();
                         break;
                     case "3":
+                        // Changement de langue (Français / Anglais)
                         _viewModel.SwitchLanguage();
                         Console.WriteLine(_viewModel.CurrentLanguage == "fr" ? "Langue changée !" : "Language changed!");
                         break;
@@ -40,7 +59,7 @@ namespace EasySave.View
                         // On appelle la méthode de nettoyage du ViewModel
                         _viewModel.ClearAllJobs();
 
-                        // Petit message de confirmation traduit
+                        // Message de confirmation de suppression
                         if (_viewModel.CurrentLanguage == "fr")
                             Console.WriteLine("Configuration supprimée. Au revoir !");
                         else
@@ -48,22 +67,28 @@ namespace EasySave.View
 
                         exit = true; // On sort de la boucle pour fermer le programme
                         break;
-                    case "q":
+                    case "q": // on quitte sans faire de clean, si on a créé des jobs
+                              // ils seront toujours dans la liste lors du prochain lancement
                         exit = true;
                         break;
                 }
             }
         }
+
+        /// <summary>
+        /// Méthode qui gère l'interface de saisie pour la création d'un travail
+        /// Contient une vérification de la cohérence des chemins sources et destinations.
+        /// </summary>
         private void CreateJobUI()
         {
             bool isFr = (_viewModel.CurrentLanguage == "fr");
             Console.WriteLine(isFr ? "\n--- Création d'un travail ---" : "\n--- Create a Backup Job ---");
 
-            // 1. NOM
+            // 1. Saisie du nom
             Console.Write(isFr ? "Nom du travail : " : "Job Name: ");
             string name = Console.ReadLine()!;
 
-            // 2. SOURCE (Doit exister physiquement)
+            // 2. Saisie de la source (Doit exister physiquement)
             Console.Write(isFr ? "Chemin source : " : "Source Path: ");
             string source = Console.ReadLine()!;
             while (string.IsNullOrWhiteSpace(source) || !System.IO.Directory.Exists(source))
@@ -73,17 +98,18 @@ namespace EasySave.View
                 source = Console.ReadLine()!;
             }
 
-            // 3. DESTINATION (Doit avoir un format valide et un lecteur existant)
+            // 3. Saisie de la destination (doit avoir un format valide et un lecteur existant)
             Console.Write(isFr ? "Chemin destination : " : "Target Path: ");
             string target = Console.ReadLine()!;
             bool isTargetValid = false;
 
+            // Si c'est valide
             while (!isTargetValid)
             {
                 try
-                {
+                {   // On vérifie que root existe ( ex Users/)
                     string? root = System.IO.Path.GetPathRoot(target);
-                    // Vérifie : Pas vide ET Chemin complet existe
+                    // Vérifiation : Pas vide ET Chemin complet existe
                     if (!string.IsNullOrWhiteSpace(target) &&
                         System.IO.Path.IsPathRooted(target) &&
                         System.IO.Directory.Exists(root))
@@ -91,6 +117,7 @@ namespace EasySave.View
                         isTargetValid = true;
                     }
                     else
+                    // Gestion des erreurs
                     {
                         Console.WriteLine(isFr
                             ? "Erreur : La destination doit être un chemin complet sur un lecteur existant."
@@ -107,14 +134,18 @@ namespace EasySave.View
                 }
             }
 
-            // 4. TYPE
+            // 4. Saisie du type
             Console.Write(isFr ? "Type (Complet/Différentiel) : " : "Type (Full/Differential): ");
             string type = Console.ReadLine()!;
 
-            // 5. ENVOI AU VIEWMODEL
+            // 5. Envoi au ViewModel pour traitement et enregistement
             _viewModel.AddJob(name, source, target, type);
             Console.WriteLine(isFr ? "Travail ajouté avec succès !" : "Job added successfully!");
         }
+
+        /// <summary>
+        /// Méthode pour gérer l'interface de lancement de sauvegarde
+        /// </summary>
         private void ExecuteJobUI()
         {
             if (_viewModel.CurrentLanguage == "fr")
@@ -127,12 +158,14 @@ namespace EasySave.View
             }
 
             string input = Console.ReadLine()!;
+
+            // Sécurisation de la saisie,pour ne pas avoir de crash au cas où l'utilsateur tape des lettres
             if (int.TryParse(input, out int index))
             {
                 // On vérifie si l'index est valide (0-4) et si le job existe
                 if (index >= 0 && index < _viewModel.Jobs.Count)
                 {
-                    // C'EST ICI QUE LA VIEW APPELLE LE VIEWMODEL
+                    // Lancement de la logique de copie du ViewModel
                     _viewModel.ExecuteJob(index);
 
                     Console.WriteLine(_viewModel.CurrentLanguage == "fr" ? "Exécution terminée." : "Execution finished.");
@@ -143,6 +176,10 @@ namespace EasySave.View
                 }
             }
         }
+
+        /// <summary>
+        /// Méthode pour afficher la liste des jobs créés
+        /// </summary>
         private void ShowJobsList()
         {
             Console.WriteLine(_viewModel.CurrentLanguage == "fr" ? "\n--- Liste des Travaux ---" : "\n--- Jobs List ---");
@@ -152,15 +189,19 @@ namespace EasySave.View
                 Console.WriteLine(_viewModel.CurrentLanguage == "fr" ? "Aucun travail configuré." : "No jobs configured.");
                 return;
             }
-
+            // Parcours de la liste du ViewModel
             for (int i = 0; i < _viewModel.Jobs.Count; i++)
             {
                 var job = _viewModel.Jobs[i];
-                // On affiche l'index, le nom et l'état
+                // On affiche l'index, le nom et la source
                 Console.WriteLine($"[{i}] Nom: {job.Name} | Source: {job.SourceDir}");
             }
         }
 
+        /// <summary>
+        /// Méthode pour afficher les options du menu principal.
+        /// Une structure condtionnelle permet d'avoir le menu en anglais ou en français
+        /// </summary>
         public void ShowMenu()
         {
             // On affiche la liste des travaux en haut du menu
