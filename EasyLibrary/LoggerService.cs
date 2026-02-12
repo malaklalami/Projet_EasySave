077b0918e54bf1f5b42bc606e3da2b5e1c0e2b0b
malaklalami <malaklalami@hotmail.fr>
2026-02-12 11:40:08 +0100

    ajout du choix du format de log XML

--- EasyLibrary/LoggerService.cs
+++ EasyLibrary/LoggerService.cs
@@ -1,31 +1,58 @@
 ﻿using System.Text.Json;
 using System.IO;
 using EasyLibrary.Models;
+using System.Xml.Serialization;
 
 namespace EasyLog;
 
 public class LoggerService
+
 {
+    public string LogFormat { get; set; } = "json";
     // On change le chemin pour qu'il crée un dossier "Logs" là où est ton projet
     // AppDomain.CurrentDomain.BaseDirectory = le dossier où ton .exe est exécuté
     private readonly string _logFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
 
     public void WriteLog(LogEntry entry)
     {
-        // 1. CE MESSAGE DOIT S'AFFICHER DANS TA CONSOLE NOIRE
+        // 1. Message de debug dans la console
         Console.WriteLine(">>>> TENTATIVE D'ECRITURE DU LOG DANS : " + _logFolder);
 
-        if (!Directory.Exists(_logFolder)) Directory.CreateDirectory(_logFolder);
+        // Créer le dossier Logs s'il n'existe pas
+        if (!Directory.Exists(_logFolder))
+        {
+            Directory.CreateDirectory(_logFolder);
+        }
 
-        string fileName = $"{DateTime.Now:yyyy-MM-dd}.json";
+        // v1.1 : On définit l'extension selon le format choisi
+        string extension = LogFormat.ToLower();
+        string fileName = DateTime.Now.ToString("yyyy-MM-dd") + "." + extension;
         string filePath = Path.Combine(_logFolder, fileName);
 
+        // On ajoute l'heure actuelle au log
         entry.Timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-        string jsonText = JsonSerializer.Serialize(entry, new JsonSerializerOptions { WriteIndented = true });
 
-        File.AppendAllText(filePath, jsonText + Environment.NewLine);
+        // 2. Logique de choix du format
+        if (extension == "json")
+        {
+            // format JSON
+            string jsonText = JsonSerializer.Serialize(entry, new JsonSerializerOptions { WriteIndented = true });
+            File.AppendAllText(filePath, jsonText + Environment.NewLine);
+        }
+        else
+        {
+            // format XML
+            XmlSerializer serializer = new XmlSerializer(typeof(LogEntry));
 
-        // 2. CE MESSAGE CONFIRME QUE LE FICHIER EST CREE
+            // On ouvre le fichier en mode "Append" (true)
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                serializer.Serialize(sw, entry);
+                sw.WriteLine(); // Pour séparer les logs
+            }
+        }
+
+        // 3. Confirmation finale
         Console.WriteLine(">>>> FICHIER CREE AVEC SUCCES : " + filePath);
     }
 }
