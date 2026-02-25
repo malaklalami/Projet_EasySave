d41f37c15e9f4dc55dd9d4d3f06bf79645fc59ad
jayjay10 <elyess.jaidane@gmail.com>
2026-02-25 16:54:14 +0100

    nouvelle branche avec retour de maximilien2

--- EasyLibrary/Models/BackupState.cs
+++ EasyLibrary/Models/BackupState.cs
@@ -5,10 +5,10 @@ namespace EasySave.Models;
 
 public class BackupState
 {
-    public string JobName { get; set; } = string.Empty;
+    public string JobName { get; set; } = string.Empty;// piste: mettre id
     public JobState Status { get; set; } = JobState.Inactive;
-    public double Progress { get; set; }
-    public string CurrentFile { get; set; } = string.Empty;
+    public double Progress { get; set; }//piste:inutile
+    public string CurrentFile { get; set; } = string.Empty;//piste :liste des fichiers a copier
     public DateTime LastUpdate { get; set; }
 }
 //Contient ce qui change pendant une sauvegarde (progression %, fichier en cours, statut). C'est ce qui va dans state.json
